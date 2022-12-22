21dd744ca25b0dbd83075cf554685f98443f0d95
EugeneSince1993 <jk235@mail.ru>
2022-12-22 16:49:29 +0300

    deploy again

--- server/src/index.js
+++ server/src/index.js
@@ -14,10 +14,10 @@ const main = async () => {
   app.use('/api', router);
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('./../../client/build'));
+    app.use(express.static('./../client/build'));
   
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, './../../client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, './../client', 'build', 'index.html'));
     });
   }  
 
